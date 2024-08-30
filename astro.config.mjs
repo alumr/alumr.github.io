6013143eb4551d68ba056d6760829f57426c1100
alumr <335759051@qq.com>
2024-08-30 18:24:13 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alumr.github.io/",
+  site: "https://shidux.cn/",
   base: "/",
   trailingSlash: "always",
   integrations: [
